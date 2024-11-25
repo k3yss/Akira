f34e06f05c7d396746ea287312fb56ff6c59010a
Rishi Kumar <rsi.dev17@gmail.com>
2024-11-25 23:58:49 +0530

    fix: Development environment

--- devenv.nix
+++ devenv.nix
@@ -1,21 +1,27 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
-	languages.javascript.enable = true;
-	languages.python.enable = true;
-
-	packages = [ 
-		pkgs.python312Packages.flask
-		pkgs.python312Packages.flask-cors
-	];
-
+  languages.javascript.enable = true;
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.requirements = "
+		flask
+		flask-cors
+	";
   processes = {
     backend.exec = "python akira-backend/main.py";
     frontend.exec = "cd akira-frontend && npm run dev";
   };
 
-	git-hooks.hooks = {
-		black.enable = true;
-		prettier.enable = true;
-	};
+  git-hooks.hooks = {
+    black.enable = true;
+    prettier.enable = true;
+    nixfmt-rfc-style.enable = true;
+  };
 }
